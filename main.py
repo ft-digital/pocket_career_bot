d9a0cef593095a23b7f3eae6b89144acf8d4ef47
vladimirzolotarev <vzolotarev@futuretoday.ru>
2022-09-29 15:13:47 +0300

    Get Today Task + Variant Buttons + Reply to answer

--- main.py
+++ main.py
@@ -3,55 +3,66 @@ import pandas as pd
 import telebot
 from telebot import types
 import datetime, time
+import os
 
-# bot_token = ''
 
 # Start bot
+bot_token = os.environ['BOT_TOKEN']
 bot = telebot.TeleBot(bot_token, parse_mode=None)
 
-# Выдать задачу для этой даты
 
-# tasks = pd.read_csv('date_task.csv', sep=';')
-# tasks['date'] = pd.to_datetime(tasks['date'])
-# pprint(tasks['date'].dt.date[2])
+# Функция чтения файла с задачами и фильтрации строки с нужной датой
+def current_date_task_data(current_date):
+    tasks = pd.read_csv('date_task.csv', sep=';')
+    tasks['date'] = pd.to_datetime(tasks['date'])
+    current_date_mask = tasks['date'].dt.date == current_date
+    current_task_row = tasks[current_date_mask]
+
+    return current_task_row
+
 
 # Welcome message
 @bot.message_handler(commands=['start'])
 def start_button(message):
     # Get today task
-    tasks = pd.read_csv('date_task.csv', sep=';')
-    tasks['date'] = pd.to_datetime(tasks['date'])
     current_date = datetime.datetime.utcfromtimestamp(message.date).date()
-    current_date_mask = tasks['date'].dt.date == current_date
-    pprint(current_date)
-    pprint(current_date_mask)
-    current_task = tasks[current_date_mask]['task']
-    pprint(current_task)
+    current_task = current_date_task_data(current_date)['task']
 
     # Buttons
-    # button_one = types.InlineKeyboardButton('1', callback_data='1')
-    # button_two = types.InlineKeyboardButton('2', callback_data='2')
-    # keyboard = types.InlineKeyboardMarkup()
-    # keyboard.add(button_one)
-    # keyboard.add(button_two)
+    keyboard = types.InlineKeyboardMarkup()
+    button_one = types.InlineKeyboardButton('1', callback_data='1')
+    button_two = types.InlineKeyboardButton('2', callback_data='2')
+    button_three = types.InlineKeyboardButton('3', callback_data='3')
+    button_four = types.InlineKeyboardButton('4', callback_data='4')
+    keyboard.add(button_one)
+    keyboard.add(button_two)
+    keyboard.add(button_three)
+    keyboard.add(button_four)
 
     # Reply to user
     pprint(current_task)
     bot.send_message(message.chat.id, current_task)
-    # msg = bot.send_message(message.chat.id, text='Выбери вариант ответа', reply_markup=keyboard)
-    # bot.register_next_step_handler(msg, process_city_step)
+    bot.send_message(message.chat.id, text='Выбери вариант ответа', reply_markup=keyboard)
 
 
-# def process_city_step(message):
-#     if not message.text.startswith('/'):
-#         bot.send_message(message.chat.id, 'You has to click one of the buttons!')
-#     start_button(message)
+@bot.callback_query_handler(func=lambda call: call.data in ['1', '2', '3', '4'])
+def longname(call):
+    cid = call.message.chat.id
+    current_date = datetime.datetime.utcfromtimestamp(call.message.date).date()
+    current_correct = str(list(current_date_task_data(current_date)['correct'])[0])
+    current_is_correct = list(current_date_task_data(current_date)['is_correct'])[0]
+    current_is_incorrect = list(current_date_task_data(current_date)['is_incorrect'])[0]
 
+    # bot.send_message(cid, call.data)
+    print(call.data)
+    print(current_correct)
+    print(call.data == current_correct)
 
-# @bot.callback_query_handler(func=lambda call: True)
-# def longname(call):
-#     if call.data == "1":
-#         bot.send_message(chat_id=call.message.chat.id, text="Нажал 1")
+    if call.data == current_correct:
+        bot.send_message(cid, current_is_correct)
+    else:
+        bot.send_message(cid, current_is_incorrect)
 
 
 bot.infinity_polling()
+
