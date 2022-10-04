f8801f8f9826471d8000c2f525458410155ebb68
Valeria <vkozunova@futuretoday.ru>
2022-10-04 14:43:15 +0300

    Запись данных и статистика

--- initial.py
+++ initial.py
@@ -3,36 +3,36 @@ from pprint import pprint
 from sqlite3 import Error
 
 
-def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
-
-
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query executed successfully")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
+# def create_connection(path):
+#     connection = None
+#     try:
+#         connection = sqlite3.connect(path)
+#         print("Connection to SQLite DB successful")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#     return connection
+#
+#
+# def execute_query(connection, query):
+#     cursor = connection.cursor()
+#     try:
+#         cursor.execute(query)
+#         connection.commit()
+#         print("Query executed successfully")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#
+#
+# def execute_read_query(connection, query):
+#     cursor = connection.cursor()
+#     result = None
+#     try:
+#         cursor.execute(query)
+#         result = cursor.fetchall()
+#         return result
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
 
 
 # Вопросы - дата, вопрос, правильный ответ, отбивка при верном ответе, отбивка при неверном ответе
@@ -42,67 +42,75 @@ def execute_read_query(connection, query):
 # connection = create_connection("bot_users_data.sqlite")
 
 # Open connection
-
-connection = sqlite3.connect('bot_users_data.sqlite')
+#
+# connection = sqlite3.connect('bot_users_data.sqlite')
 
 # Create question table
 
-create_questions_table = """
-CREATE TABLE IF NOT EXISTS questions (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  date TEXT,
-  question TEXT,
-  right_answer TEXT,
-  correct_reply TEXT,
-  incorrect_reply TEXT
-);
-"""
+# create_questions_table = """
+# CREATE TABLE IF NOT EXISTS questions (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   date TEXT,
+#   question TEXT,
+#   right_answer TEXT,
+#   correct_reply TEXT,
+#   incorrect_reply TEXT
+# );
+# """
 
 # execute_query(connection, create_questions_table)
 
 # Create answers  table
-
-create_answers_table = """
-CREATE TABLE IF NOT EXISTS answers(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  date TEXT,
-  tg_id TEXT,
-  is_right INT,
-  start_datetime INT,
-  end_datetime INT
-);
-"""
+#
+# create_answers_table = """
+# CREATE TABLE IF NOT EXISTS answers(
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   date TEXT,
+#   tg_id TEXT,
+#   is_right INT,
+#   start_datetime INT,
+#   end_datetime INT
+# );
+# """
 
 # execute_query(connection, create_answers_table)
-
-create_questions = """
-INSERT INTO
-  questions (date, question, right_answer, correct_reply, incorrect_reply)
-VALUES
-  ('2022-10-01', 'question_1', '2', 'good boy', 'bad boy'),
-  ('2022-10-02', 'question_2', '1', 'good boy 2', 'bad boy 2');
-"""
+#
+# create_questions = """
+# INSERT INTO
+#   questions (date, question, right_answer, correct_reply, incorrect_reply)
+# VALUES
+#   ('2022-10-01', 'question_1', '2', 'good boy', 'bad boy'),
+#   ('2022-10-02', 'question_2', '1', 'good boy 2', 'bad boy 2');
+# """
 
 # execute_query(connection, create_questions)
 
 
 # Test table - id, name, answer
-create_test_table = """
-CREATE TABLE IF NOT EXISTS test (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  tg_id TEXT,
-  tg_name TEXT,
-  answer TEXT
-);
-"""
+# create_test_table = """
+# CREATE TABLE IF NOT EXISTS test (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   tg_id TEXT,
+#   tg_name TEXT,
+#   answer TEXT
+# );
+# """
 
 # execute_query(connection, create_test_table)
 
 
 # EXECUTE QUERY
+#
+# query = """
+# SELECT * FROM test
+# """
+#
+# pprint(execute_read_query(connection, query))
 
-query = """
-SELECT * FROM test
-"""
 
-pprint(execute_read_query(connection, query))
+# # Подключаемся к ДБ
+# conn = sqlite3.connect('users_info.db')
+# c = conn.cursor()
+
+# # Создаем таблицу с данными пользователей
+# c.execute('CREATE TABLE IF NOT EXISTS users (user_id INT, date TEXT, tg_id TEXT, answer INT, is_correct INT, start_time TEXT, end_time TEXT, duration INT)')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 import datetime, time
 import os
-
+import sqlite3
 
 class User:
     def __int__(self):
@@ -13,7 +13,6 @@ class User:
 # Start bot
 bot_token = os.environ['BOT_TOKEN']
 bot = telebot.TeleBot(bot_token, parse_mode=None)
-
 
 # Функция чтения файла с задачами и фильтрации строки с нужной датой
 def current_date_task_data(current_date):
@@ -24,14 +23,12 @@ def current_date_task_data(current_date):
 
     return current_task_row
 
-
 # Welcome message
 @bot.message_handler(commands=['start'])
 def start_button(message):
     start_time = int(time.time())
 
     # Get today task
-
     current_date = datetime.datetime.utcfromtimestamp(message.date).date()
     current_task = current_date_task_data(current_date)['task']
     bot.send_message(message.chat.id, current_task)
@@ -42,9 +39,9 @@ def start_button(message):
     # Buttons
     keyboard = types.InlineKeyboardMarkup()
     button_one = types.InlineKeyboardButton('1', callback_data=callback_data_list[0])
-    button_two = types.InlineKeyboardButton('2', callback_data='2')
-    button_three = types.InlineKeyboardButton('3', callback_data='3')
-    button_four = types.InlineKeyboardButton('4', callback_data='4')
+    button_two = types.InlineKeyboardButton('2', callback_data=callback_data_list[1])
+    button_three = types.InlineKeyboardButton('3', callback_data=callback_data_list[2])
+    button_four = types.InlineKeyboardButton('4', callback_data=callback_data_list[3])
     keyboard.add(button_one)
     keyboard.add(button_two)
     keyboard.add(button_three)
@@ -58,6 +55,21 @@ def start_button(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def longname(call):
+    # Подключаемся к ДБ
+    conn = sqlite3.connect('users_info.db')
+    c = conn.cursor()
+
+    # Функция создания таблицы с данными пользователей
+    def create_table():
+        c.execute(
+            'CREATE TABLE IF NOT EXISTS users_info (user_id INT, date TEXT, us_answer INT, is_correct INT, start_time TEXT, end_time TEXT, duration INT)')
+
+    # Функция записи данных в таблицу users
+    def data_entry():
+        c.execute('INSERT INTO users_info (user_id, date, us_answer, is_correct, start_time, end_time, duration) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            (user_id, date, us_answer, is_correct, start_time, end_time, duration))
+        conn.commit()
+
     start_time = int(call.data.split("|")[1])
     answer = call.data.split("|")[0]
     cid = call.message.chat.id
@@ -79,8 +91,70 @@ def longname(call):
 
     if answer == current_correct:
         bot.send_message(cid, current_is_correct)
+        is_correct = 1
     else:
         bot.send_message(cid, current_is_incorrect)
+        is_correct = 0
+
+    #Записываем данные в таблицу
+
+    user_id = cid
+    date = current_date
+    us_answer = answer
+
+    data_entry()
+    c.close()
+    conn.close()
+
+#Statistics message
+@bot.message_handler(commands=['stats'])
+def stats_button(message):
+    tg_id = message.chat.id
+    # Подключаемся к ДБ
+    conn = sqlite3.connect('users_info.db')
+    c = conn.cursor()
+
+    # Задачи
+    def get_right_answers(us_id):
+        query = """
+        SELECT is_correct FROM users_info
+        WHERE user_id LIKE '%{tg_id}%'
+        """.format(tg_id=tg_id)
+        is_right = c.execute(query)
+
+        sum_right = 0
+        for point in is_right:
+            sum_right += point[0]
+        len_tasks = len(is_right.fetchall())
+
+        return (sum_right, len_tasks)
+
+    task_message = "Правильно решенных задач: " + str(get_right_answers(tg_id)[0]) + "\n\n" + "Всего задач: " + str(get_right_answers(tg_id)[1])
+
+    # Время
+    def get_av_time(us_id):
+        query = """
+        SELECT duration FROM users_info
+        WHERE user_id LIKE '%{tg_id}%'
+        """.format(tg_id=tg_id)
+        dur = c.execute(query)
+
+        sum_dur = 0
+        for sec in dur:
+            sum_dur += sec[0]
+
+        return sum_dur
+        # return (sum_dur / dur.count())
+
+
+
+    av_time_message = "\n\n" + "Среднее время на решение одной задачи: " + str(get_av_time(tg_id))
+    stats_message = task_message + av_time_message
+
+    bot.send_message(tg_id, stats_message)
+
+    c.close()
+    conn.close()
 
 
 bot.infinity_polling()
